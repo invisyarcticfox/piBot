5eb1314c3fd10228af6a6de4379b6d1e87bba82f
Lucas. <iaminvisy@gmail.com>
2025-12-03 03:47:43 +0000

    sorted files and stuff

---
+++ src/api/index.ts
@@ -0,0 +1,49 @@
+import express from 'express'
+import { fetchMember, lastOnline, userId } from '../bot'
+
+export const app = express()
+
+
+app.get('/discord', async (_req, res) => {
+  try {
+    const member = await fetchMember(userId)
+    res.json({
+      id: member.user.id,
+      name: member.user.globalName,
+      username: member.user.username,
+      status: member.presence?.status ?? 'offline',
+      lastOnline,
+      avatar: member.user.displayAvatarURL({ size: 1024, extension: 'png' })
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
+app.get('/discord/avatar', async (req, res) => {
+  try {
+    const member = await fetchMember(userId)
+    const allowedExts = ['png', 'jpg', 'jpeg', 'webp', 'gif']
+    let format = (req.query.f as string)?.toLowerCase()
+    if (!allowedExts.includes(format)) format = 'png'
+
+    const avatarUrl = member.user.displayAvatarURL({
+      size: 1024,
+      extension: format as any,
+      forceStatic: format !== 'gif'
+    })
+
+    if ('r' in req.query) return res.redirect(avatarUrl)
+
+    const img = await fetch(avatarUrl)
+    const buffer = Buffer.from(await img.arrayBuffer())
+
+    res.setHeader('Content-Type', format === 'jpg' || format === 'jpeg' ? 'image/jpeg' : `image/${format}`)
+    res.setHeader('Cache-Control', 'public, max-age=1800')
+    return res.send(buffer)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
---
+++ src/bot/index.ts
@@ -0,0 +1,46 @@
+import 'dotenv/config'
+import { ActivityType, Client, GatewayIntentBits, type GuildMember } from 'discord.js'
+
+const token = process.env.BOT_TOKEN
+const guildId = '939964758805872650'
+export const userId = '470193291053498369'
+export let lastOnline:string|null = null
+export const botStart = new Date(Date.now()).toLocaleString('en-GB', { timeZone: 'UTC' })
+
+export const client = new Client({
+  intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences ]
+})
+
+
+client.once('clientReady', () => {
+  console.log(`Logged in as ${client.user?.username}.`)
+  client.user?.setPresence({
+    activities: [{
+      type: ActivityType.Custom,
+      name: 'custom status',
+      state: `Online since ${botStart.slice(0,-3)}`
+    }],
+    status:'online'
+  })
+})
+
+client.on('presenceUpdate', (oldPresence, newPresence) => {
+  const oldStatus = oldPresence?.status ?? 'offline'
+  const newStatus = newPresence?.status as string
+  const onlineStatuses = [ 'online', 'idle', 'dnd' ]
+  
+  if  (onlineStatuses.includes(oldStatus) && !onlineStatuses.includes(newStatus)) {
+    lastOnline = new Date().toISOString()
+  } else if (!onlineStatuses.includes(oldStatus) && onlineStatuses.includes(newStatus)) {
+    lastOnline = null
+  }
+})
+
+
+export async function fetchMember(user:string=userId):Promise<GuildMember> {
+  let guild = client.guilds.cache.get(guildId) || await client.guilds.fetch(guildId)
+  let member = guild.members.cache.get(user) || await guild.members.fetch(user)
+  return member
+}
+
+export async function startBot() { await client.login(token) }
